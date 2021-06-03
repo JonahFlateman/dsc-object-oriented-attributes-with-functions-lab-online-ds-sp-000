d179a0b790802ce7ee09d5daf095c863092c1019
Jonah Flateman <jonahflateman@gmail.com>
2021-06-03 09:45:27 -0400

    Completed 6/3/21 9:45am

--- school.py
+++ school.py
@@ -1,1 +1,24 @@
-class School:
+import copy
+
+class School():
+    def __init__(self, name):
+        self.name = name
+        self.roster = {}
+
+    def add_student(self, name, grade):
+        if grade in self.roster:
+            self.roster[grade].append(name)
+        else:
+            self.roster[grade] = [name]
+        return self.roster
+    
+    def grade(self, grade):
+        return self.roster[grade]
+    
+    def sort_roster(self):
+        new_dict = copy.deepcopy(self.roster)
+        for key in new_dict:
+            new_dict[key].sort()
+        return new_dict
+            
+    
